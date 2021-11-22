ac6685f78496c924d7b32455ef0b3154f4aea48c
Sue Lin <r.lin13@knights.ucf.edu>
2021-11-21 21:10:34 -0500

    added copyright statements to some tests

--- InventoryManagementApplication/src/test/java/ApplicationControllerTest.java
+++ InventoryManagementApplication/src/test/java/ApplicationControllerTest.java
@@ -1,6 +1,10 @@
+/*
+ *  UCF COP3330 Fall 2021 Application Assignment 2 Solution
+ *  Copyright 2021 Sue Lin
+ */
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.control.TextField;
 import org.junit.jupiter.api.Test;
 
 import java.util.Random;
